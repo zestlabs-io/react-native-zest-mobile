2953b3aafaec4ff040e4b043eecff660a7e75092
Nick Penkov <nick.penkov@gmail.com>
2020-07-18 18:04:37 +0200

    Expose more interfaces

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,9 @@
 import { AuthBridge, AuthLoggedIn, AuthLoggedOut, AuthUserChanged } from './authBridge';
-import { SyncManager, SyncDB, SyncChange, SyncPaused, SyncActive, SyncDenied, SyncComplete, SyncError } from './syncManager';
+import { SyncManager, SyncDB, SyncChange, SyncPaused, SyncActive, SyncDenied, SyncComplete, SyncError, SyncConfig, SyncEvent } from './syncManager';
+import ZDB from './zdb';
 export { AuthBridge };
 export { AuthLoggedIn, AuthLoggedOut, AuthUserChanged };
 export { SyncManager, SyncDB };
 export { SyncChange, SyncPaused, SyncActive, SyncDenied, SyncComplete, SyncError };
+export { SyncConfig, SyncEvent };
+export { ZDB };
--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,15 @@ import {
   SyncDenied,
   SyncComplete,
   SyncError,
+  SyncConfig,
+  SyncEvent
 } from './syncManager';
+import ZDB from './zdb';
 
 export { AuthBridge };
 export { AuthLoggedIn, AuthLoggedOut, AuthUserChanged };
 export { SyncManager, SyncDB };
 export { SyncChange, SyncPaused, SyncActive, SyncDenied, SyncComplete, SyncError };
+export { SyncConfig, SyncEvent };
+export { ZDB };
 
