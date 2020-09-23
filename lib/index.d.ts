46a021f42e9860e3bd86fcfd4e805558fd1d64fe
Nick Penkov <nick.penkov@gmail.com>
2020-09-23 17:26:25 +0200

    Cleanup

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthBridge, AuthLoggedIn, AuthLoggedOut, AuthUserChanged } from './authBridge';
-import { SyncManager, SyncDB, SyncChange, SyncPaused, SyncActive, SyncDenied, SyncComplete, SyncError, SyncConfig, SyncEvent } from './syncManager';
-import ZDB from './zdb';
-export { AuthBridge };
-export { AuthLoggedIn, AuthLoggedOut, AuthUserChanged };
-export { SyncManager, SyncDB };
-export { SyncChange, SyncPaused, SyncActive, SyncDenied, SyncComplete, SyncError };
-export { SyncConfig, SyncEvent };
-export { ZDB };
